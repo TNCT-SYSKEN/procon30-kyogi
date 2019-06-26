7f11853e92c03bb0c7da3f362ce5c6d276918fb4
takishun <takishun.st3260@gmail.com>
2019-06-26 22:00:13 +0900

    general.hとfield.hの追加

---
+++ procon30_kyogi/procon30_kyogi/general.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Siv3D.hpp>
+#include <iostream>
+#include <utility>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+//このファイルは去年の先輩のものとほぼ同じ
+//アルゴリズム計算などに参照する予定
+
