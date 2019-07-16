ad465e5fe98f14b3a5bc1a8b5c7ae447d1df76b2
takishun <takishun.st3260@gmail.com>
2019-07-16 15:40:04 +0900

    Field_test.hの変更

--- procon30_kyogi/procon30_kyogi/algorithm/Field_test.h
+++ procon30_kyogi/procon30_kyogi/algorithm/Field_test.h
@@ -3,36 +3,7 @@
 
 class Field_test {
 public:
-
-	//ここでフィールド情報をテスト用で定義
-
-	int *width;
-	int *height;
-	//teamID
-	int *my_teamID;
-	int *other_teamID;
-
-	//ポイント情報
-	static vector<vector<int>>points;
-
-	
-
-
-
-	//エージェントID,x,y
-	static vector<vector<int>>*agents;
-
-	//タイル状況
-	static vector<vector<int>>*tiled;
-
-	//タイルポイント,領域ポイント,合計ポイント
-	//自分のチーム
-	static vector<int>*my_team_point;
-
-	//相手のチーム
-	static vector<int>*other_team_point;
-
-	static void Field();
+	static void fieldTest();
 };
 
 
