c890fd81b43eeca9782af38e606682ac70b08837
takishun <takishun.st3260@gmail.com>
2019-07-17 22:17:47 +0900

    移動した後のタイルポイントを加算する関数の追加

--- procon30_kyogi/procon30_kyogi/algorithm/Hyouka.cpp
+++ procon30_kyogi/procon30_kyogi/algorithm/Hyouka.cpp
@@ -9,31 +9,41 @@
 //自分のチームの得点、相手のチームの得点
 //それぞれを引数として扱う
 
-vector<vector<int>> Hyouka::hyouka_keisan(vector<vector<int>>field, vector<vector<int>>hoge, vector<vector<int>>agents, vector<int>my_team_point, vector<int>other_team_point)
+vector<vector<int>> Hyouka::hyoukaKeisan()
 {
-	//フィールド情報		field
-	//タイル状況			tiled
-	//エージェントの位置	agents
-	//自得点				my_team_point
-	//相得点				other_team_point
+	Map *map;
+	map = map->getMap();
+
+	Hyouka::hyoukaPoint={};//評価点初期化
 
 
-	Hyouka::hyouka_point={};//評価点初期化
-
-	int agents_num = agents[0][0];
 	int hyouka_p;
 	
 
-	int max_value = 0;//最大評価点
-	//エージェントの数だけ評価点計算
-	rep(i, agents.size()) {
-		hyouka_p = 0;
-		
-
-	}
+	
 
 	
 
 	return vector<vector<int>>();
 }
+
+int Hyouka::calculateScore(int x,int y)
+{
+	Map *map;
+	map = map->getMap();
+	Field *field;
+	field = field->getField();
+
+	int sum = 0;
+
+	if (x <= 0 || x > map->width || y <= 0 || y > map->vertical) {
+		return -1;
+	}
+	else {
+		field->points[x - 1][y - 1];
+	}
+
+	return sum;
+}
+
 
--- procon30_kyogi/procon30_kyogi/algorithm/Hyouka.h
+++ procon30_kyogi/procon30_kyogi/algorithm/Hyouka.h
@@ -1,14 +1,14 @@
 #pragma once
-#include"Action_manager.h"
+#include"ActionManager.h"
 
 class Hyouka {
 public:
-	//評価点計算に必要な情報は
-	//フィールド情報、タイル状況、エージェントの位置
-	//自分のチームの得点、相手のチームの得点
-	//それぞれを引数として扱う
 
-	vector<vector<int>>hyouka_keisan(vector < vector<int>>, vector<vector<int>>, vector<vector<int>>, vector<int>, vector<int>);
-	vector<vector<int>>hyouka_point;//評価点が入る
+	vector<vector<int>>hyoukaKeisan();
+	vector<vector<int>>hyoukaPoint;//評価点が入る
+
+	int calculateScore(int,int);
+
+
 
 };
