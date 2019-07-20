0f5fe03ca901cc9692b1bc2e632cf7642d5debdd
takishun <takishun.st3260@gmail.com>
2019-07-20 21:52:44 +0900

    先読みタイルの点数加算実装

--- procon30_kyogi/procon30_kyogi/algorithm/Hyouka.cpp
+++ procon30_kyogi/procon30_kyogi/algorithm/Hyouka.cpp
@@ -1,16 +1,6 @@
 #include"Hyouka.h"
 
 
-//全てのエージェントについて一気に評価点計算をしたほうが
-//場所変更などをしやすい
-
-//評価点計算に必要な情報は
-//フィールド情報、タイル状況、エージェントの位置
-//自分のチームの得点、相手のチームの得点
-//それぞれを引数として扱う
-
-
-vector<vector<>>route = {};//経路初期化
 
 int dx[] = { 1,1,1,0,0,0,-1,-1,-1 };
 int dy[] = { 1,0,-1,1,0,-1,1,0,-1 };
@@ -24,15 +14,18 @@ vector<vector<int>> Hyouka::hyoukaKeisan()
 
 	pair<int, int>agentPosition;
 	
-	
+	//agentnumber,<agentpositionX,Y>
+	vector<pair<int, pair<int, int>>>route = {};//経路初期化
 
 	 //エージェントの数だけループ
 	for (int agentsnum = agents->ourAgents[0][0]; agentsnum < agents->ourAgents[0][agentsnum-1];agentsnum++) {
 		
 		agentPosition.first = agents->ourAgents[1][agentsnum-agents->ourAgents[0][0]];//x
 		agentPosition.second = agents->ourAgents[2][agentsnum-agents->ourAgents[0][0]];//y
-
-		calculateEvalution(agentsnum,agentPosition,map->readTurn,0);
+		//agentの初期位置
+		route.push_back(make_pair(agentsnum,agentPosition));
+		
+		calculateEvalution(route,agentsnum,agentPosition,map->readTurn,0);
 	}
 
 	
@@ -44,7 +37,7 @@ vector<vector<int>> Hyouka::hyoukaKeisan()
 
 
 //point計算
-int Hyouka::calculateScore(int x,int y)
+int Hyouka::calculateScore(pair<int,int>agentPosition)
 {
 	Map *map;
 	map = map->getMap();
@@ -53,28 +46,38 @@ int Hyouka::calculateScore(int x,int y)
 
 	int sum = 0;
 
-	if (x <= 0 || x > map->width || y <= 0 || y > map->vertical) {
+	if (agentPosition.first <= 0 || agentPosition.first > map->width || agentPosition.second <= 0 || agentPosition.second > map->vertical) {
 		return -1;
 	}
 	else {
-		field->points[x - 1][y - 1];
+		sum = field->points[agentPosition.first - 1][agentPosition.second - 1];
 	}
 
 	return sum;
 }
 
 //経路計算
-vector<int> Hyouka::calculateEvalution(int agentnum,pair<int,int>agentPosition,int readTurn,int turn)
+vector<int> Hyouka::calculateEvalution(vector<pair<int,pair<int,int>>>route,int agentnum,pair<int,int>agentPosition,int readTurn,int sum)
 {		
 	Map* map;
 	map = map->getMap();
-	int toX, toY;
 	
 	rep(turn,9) {
-		toX += dx[turn];
-		toY += dy[turn];
-		agentPosition.first
-		calculateEvalution(agentnum,)
+		agentPosition.first += dx[turn];
+		agentPosition.second += dy[turn];
+		route.push_back(make_pair(agentnum, agentPosition));
+		if (turn == 4) {}//移動しない
+		else {
+			sum += calculateScore(agentPosition);
+		}
+		if (readTurn >= 0) {
+			readTurn--;
+			calculateEvalution(route, agentnum,agentPosition,readTurn,sum);
+		
+		}
+		else {
+
+		}
 	}
 	return vector<int>();
 }
