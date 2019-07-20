43e4eb4e240b2bb5447195a49e827ed4dcbb88cd
takishun <takishun.st3260@gmail.com>
2019-07-20 15:12:07 +0900

    agentの数手先読みの関数定義

--- procon30_kyogi/procon30_kyogi/algorithm/Hyouka.cpp
+++ procon30_kyogi/procon30_kyogi/algorithm/Hyouka.cpp
@@ -9,16 +9,31 @@
 //自分のチームの得点、相手のチームの得点
 //それぞれを引数として扱う
 
+
+vector<vector<>>route = {};//経路初期化
+
+int dx[] = { 1,1,1,0,0,0,-1,-1,-1 };
+int dy[] = { 1,0,-1,1,0,-1,1,0,-1 };
 vector<vector<int>> Hyouka::hyoukaKeisan()
 {
 	Map *map;
 	map = map->getMap();
-
-	Hyouka::hyoukaPoint={};//評価点初期化
-
-
-	int hyouka_p;
+	Agents* agents;
+	agents = agents->getAgents();
 	
+
+	pair<int, int>agentPosition;
+	
+	
+
+	 //エージェントの数だけループ
+	for (int agentsnum = agents->ourAgents[0][0]; agentsnum < agents->ourAgents[0][agentsnum-1];agentsnum++) {
+		
+		agentPosition.first = agents->ourAgents[1][agentsnum-agents->ourAgents[0][0]];//x
+		agentPosition.second = agents->ourAgents[2][agentsnum-agents->ourAgents[0][0]];//y
+
+		calculateEvalution(agentsnum,agentPosition,map->readTurn,0);
+	}
 
 	
 
@@ -27,6 +42,8 @@ vector<vector<int>> Hyouka::hyoukaKeisan()
 	return vector<vector<int>>();
 }
 
+
+//point計算
 int Hyouka::calculateScore(int x,int y)
 {
 	Map *map;
@@ -46,4 +63,19 @@ int Hyouka::calculateScore(int x,int y)
 	return sum;
 }
 
+//経路計算
+vector<int> Hyouka::calculateEvalution(int agentnum,pair<int,int>agentPosition,int readTurn,int turn)
+{		
+	Map* map;
+	map = map->getMap();
+	int toX, toY;
+	
+	rep(turn,9) {
+		toX += dx[turn];
+		toY += dy[turn];
+		agentPosition.first
+		calculateEvalution(agentnum,)
+	}
+	return vector<int>();
+}
 
