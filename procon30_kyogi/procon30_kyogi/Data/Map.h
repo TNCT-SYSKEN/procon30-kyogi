6b63ff74b4b046565b323429887779f18fd7c463
takishun <takishun.st3260@gmail.com>
2019-09-29 21:46:19 +0900

    vector初期化の訂正

--- procon30_kyogi/procon30_kyogi/Data/Map.h
+++ procon30_kyogi/procon30_kyogi/Data/Map.h
@@ -9,22 +9,28 @@ public:
 	//横
 	int width;
 
+	//Map(): vSize(3,0) {};
+	Map() : score(2, vector<int>(3,0)) {};
+	//vector<int> vSize;
 	//自分のチームのスコア{総合点,タイル,領域},
 	//相手				  {総合点,タイル,領域}
 	vector<vector<int>> score;
 	
 	//ターン数
-	int turn;
+	int turn = 0;
 
-	int readTurn = 3;
+	int readTurn;
 
 	//自分チームID
 	int ourTeamID;
 	//相手チームID
 	int otherTeamID;
 
+
+
 	//計算したかどうか
 	bool turnFlg;
+	bool isGameStarted;
 
 private:
 	static Map* stayMap;
