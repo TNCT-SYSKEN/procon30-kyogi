944037ed58725f2d2b73b1b57ffb329e12faae80
takishun <takishun.st3260@gmail.com>
2019-09-30 17:10:55 +0900

    agentの評価計算を行うための準備の実装

--- procon30_kyogi/procon30_kyogi/Data/AgentsEvalution.h
+++ procon30_kyogi/procon30_kyogi/Data/AgentsEvalution.h
@@ -6,6 +6,10 @@ public:
 	vector<pair<int, int>> maxRoute;
 	float maxEvalutionPoint;
 
+	//タイル除去チェック
+	//nターン先読み JudgeEvalutionで要素数は定義
+	vector < int > tileMoveupCheck;
+
 	vector<pair<int, int>>enemyMaxRoute;
 	float enamyMaxEvalutionPoint;
 
--- procon30_kyogi/procon30_kyogi/algorithm/Judge.cpp
+++ procon30_kyogi/procon30_kyogi/algorithm/Judge.cpp
@@ -11,12 +11,14 @@
 
 
 
+//注意
+//踏んだ後のタイルをもう一度取るかもしれない
+//
+
 
 //全探索モード
 void Judge::fullSearch() {
 
-	int dx[] = { 1,1,1,0,0,0,-1,-1,-1 };
-	int dy[] = { 1,0,-1,1,0,-1,1,0,-1 };
 
 	Map* map;
 	map = map->getMap();
@@ -29,8 +31,11 @@ void Judge::fullSearch() {
 
 
 	//敵ルート
-	vector<pair<int,pair<int,int>>>enemyRoute = {};
+	vector<pair<int,pair<int,int>>>enemyRoute(1);
 	int ourAgentsS = agents->ourAgents.size();
+
+	//タイル除去したマスの座標、先読みターン数の記録
+	vector<pair<int, pair<int, int>>>moveUpTile;
 
 	pair<int,int>agentsPosition;
 	for (int agentsnum = agents->otherAgents[0][0]; agentsnum < agents->otherAgents[0][0]; agentsnum++) {
@@ -40,9 +45,25 @@ void Judge::fullSearch() {
 
 		enemyRoute.push_back(make_pair(agentsnum, agentsPosition));
 		//敵最適ルート初期化
-		//	ここで先読みターン数に応じたサイズを初期化する
+		
+		/*	ここで先読みターン数に応じたサイズを初期化する	*/
+		agentsEvalution->maxRoute.resize(map->finalTurn - map->turn + 1);
+		agentsEvalution->maxEvalutionPoint = 0;
 		agentsEvalution->enemyMaxRoute.resize(0);
-		agentsPosition=
+		agentsEvalution->enamyMaxEvalutionPoint = 0;
+		
+
+		
+
+
+		
+		//行動予測
+		calculateEnemyRoute(enemyRoute, agentsPosition,moveUpTile ,0,map->finalTurn-map->turn+1, 0);
+
+
+
+
+
 
 	}
 
@@ -50,24 +71,16 @@ void Judge::fullSearch() {
 
 }
 
-void Judge::calculateTilePoint(vector<pair<int, pair<int, int>>>route,int teamID) {
+void Judge::calculateTilePoint(vector<pair<int, pair<int, int>>>route, int teamID) {
 	Map* map;
 	map = map->getMap();
 	Field* field;
 	field = field->getField();
-	
+
 	int sum = 0;
 
 
 
-	//残りのターン数だけ計算
-	rep(counter,map->finalTurn - map->turn+1)
-		rep(i, map->width) {
-			rep(j, map->vertical) {
-				if()
-				
-			}
-		}
 
 }
 
@@ -81,8 +94,67 @@ void Judge::calculateAreaPoint(vector<pair<int,pair<int,int>>>route,int teamID,i
 }
 
 
-void Judge::calculateEnemyEvalution(vector<pair<int, pair<int, int>>>route, int sum) {
+void Judge::calculateEnemyRoute(vector<pair<int, pair<int, int>>>route, pair<int, int> nowAgentPosition,
+	vector<pair<int, pair<int, int>>>moveUpTile, int moveup, int readTurn, int sum) {
+	Map* map;
+	map = map->getMap();
+	Field* field;
+	field = field->getField();
 
+
+	int dx[] = { 1,1,1,0,0,0,-1,-1,-1 };
+	int dy[] = { 1,0,-1,1,0,-1,1,0,-1 };
+
+	rep(turn, 9) {
+		
+
+		if (nowAgentPosition.first + dx[turn] < 0 || nowAgentPosition.first + dx[turn] >= map->width
+			|| nowAgentPosition.second + dy[turn] < 0 || nowAgentPosition.second + dy[turn] >= map->vertical) {
+			goto CantGoThere;
+		}
+		//踏んだのが敵（自身）のタイルだったらアクセスしない
+		if (field->tiled[nowAgentPosition.first + dx[turn]][nowAgentPosition.second + dy[turn]] == map->otherTeamID) {
+			goto CantGoThere;
+		}
+
+		if (turn == 4) {
+
+		}
+		else {
+
+
+			if (field->tiled[nowAgentPosition.first + dx[turn]][nowAgentPosition.second + dy[turn]] == map->ourTeamID) {
+				//moveupのresize
+				moveUpTile.resize(moveup + 1);
+				moveUpTile[moveup] = make_pair(map->finalTurn - map->turn + 2 - readTurn,
+					(nowAgentPosition.first + dx[turn], nowAgentPosition.second + dy[turn]));
+				moveup++;
+				
+			}
+			else {
+
+				nowAgentPosition.first += dx[turn];
+				nowAgentPosition.second += dy[turn];
+			}	
+		}
+		//routeサイズ調整
+		route.resize(route.size()+1);
+		route.push_back(make_pair(route[0].first, make_pair(dx[turn], dy[turn])));
+
+		if (readTurn > 0) {
+			readTurn--;
+			calculateEnemyRoute(route, nowAgentPosition, moveUpTile, moveup, readTurn, sum);
+		}
+		else {
+			JudgeEvalution judgeEvalution;
+			judgeEvalution.calculateEnemyEvalution(route, moveUpTile, moveup, sum);
+		}
+
+	}
+
+
+
+	CantGoThere:;
 }
 
 
