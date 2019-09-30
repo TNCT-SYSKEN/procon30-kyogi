cd94d00abcfa2b6ad424f13dd9b9292b8680a9d5
takishun <takishun.st3260@gmail.com>
2019-09-30 17:09:38 +0900

    JudgeEvalution 全探索評価の追加

---
+++ procon30_kyogi/procon30_kyogi/algorithm/JudgeEvalution.cpp
@@ -0,0 +1,6 @@
+#include"JudgeEvalution.h"
+
+void JudgeEvalution::calculateEnemyEvalution(vector<pair<int, pair<int,int>>>route,
+	vector<pair<int, pair<int,int>>>moveUpTile,int moveup,int sum) {
+
+}
---
+++ procon30_kyogi/procon30_kyogi/algorithm/JudgeEvalution.h
@@ -0,0 +1,8 @@
+#pragma once
+#include"Action_manager.h"
+
+class JudgeEvalution {
+public:
+	void calculateEnemyEvalution(vector<pair<int, pair<int,int>>>,vector<pair<int, pair<int,int>>>,int,int);
+
+};
