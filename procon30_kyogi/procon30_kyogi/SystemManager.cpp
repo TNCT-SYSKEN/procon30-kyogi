cec81d60b675870e40bafe0943cf4e6c2bf27ed3
takishun <takishun.st3260@gmail.com>
2019-10-03 01:17:11 +0900

    debug用領域ポイント計算

--- procon30_kyogi/procon30_kyogi/SystemManager.cpp
+++ procon30_kyogi/procon30_kyogi/SystemManager.cpp
@@ -24,8 +24,68 @@ void SystemManager::systemManager() {
 	}
 }
 
-void calcAreaDubug() {
+//デバッグ用の領域計算
+void calcAreaDubug(int teamID) {
 
+	Map* map;
+	map = map->getMap();
+	Field* field;
+	field = field->getField();
+
+	int dx[] = { 1,0,0,-1 };
+	int dy[] = { 0,1,-1,0 };
+
+
+	vector<vector<int>> visited(map->width+2, vector<int>(map->vertical+2, 0));
+	rep(i, map->width) {
+		rep(j, map->width) {
+			if (field->tiled[i][j] == teamID) {
+				visited[i + 1][j + 1] = teamID;
+			}
+		}
+	}
+
+	visited[0][0] = -1;
+
+	vector<pair<int, int>>search(1);
+	vector<pair<int, int>>stac;
+	search.push_back(make_pair(0, 0));
+	int searchSize = 1;
+	int searchX = 0;
+	int searchY = 0;
+
+
+	while (searchSize > 0) {
+		rep(i, searchSize) {
+			rep(turn, 4) {
+				
+				searchX = search[i].first + dx[turn];
+				searchY = search[i].second + dy[turn];
+
+				if (searchX > 0 && searchX <map->width+2 && searchY>0 && searchY <map->vertical+2) {
+					if (visited[searchX][searchY]==0) {
+						visited[searchX][searchY] = -1;
+						stac.push_back(make_pair(searchX, searchY));
+					}
+				}
+			}
+		}
+
+		search.resize(0);
+		search = stac;
+		searchSize = search.size();
+	}
 	
+	int sum = 0;
+	rep(i, map->width) {
+		rep(j, map->vertical) {
+			if (visited[i+1][j+1] == 0) {
+
+				sum += field->points[i][j];
+			}
+		}
+	}
+
+	return sum;
 
 }
--- procon30_kyogi/procon30_kyogi/SystemManager.h
+++ procon30_kyogi/procon30_kyogi/SystemManager.h
@@ -10,5 +10,5 @@ public:
 	void systemManager();
 
 private:
-	void calcAreaDebug();
+	void calcAreaDebug(int);
 };
