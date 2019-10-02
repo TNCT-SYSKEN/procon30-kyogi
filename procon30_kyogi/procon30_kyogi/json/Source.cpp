5dd997637abbcd7749012cc2ab80d6e06f4378c0
tomosue1 <nikoniko04211970@gmail.com>
2019-10-02 21:51:36 +0900

    配列の追加

--- procon30_kyogi/procon30_kyogi/json/Source.cpp
+++ procon30_kyogi/procon30_kyogi/json/Source.cpp
@@ -3,9 +3,13 @@
 #include <sstream>
 #include <string>
 #include <iterator>
+#include <cassert>
 #include "picojson.h"
 //C:\Users\友末　智将\Desktop\test - json
 //C:\Users\友末　智将\Downloads\picojson - master\picojson - master
+using namespace std;
+using namespace picojson;
+
 int main(void)
 {
 	// JSONデータの読み込み。
@@ -25,16 +29,49 @@ int main(void)
 		return 2;
 	}
 
+	//picojson::array& ary = obj["points"].get<picojson::array>(); 
+	//cout << "points:" << endl;
+	/*for (const auto& e : ary) {  // vectorをrange-based-forでまわしている。
+		std::cout << e.get<std::string>() << " ";
+	}*/
+	
+	/*int main() {
+		// Declaration
+		stringstream ss;
+		ifstream     f;
+		unsigned int i;
+
+		// Read Json file
+		f.open("フィールド情報_turn0.json", ios::binary);
+		if (!f.is_open()) return 1;
+		ss << f.rdbuf();
+		f.close();
+
+		// Parse Json data
+		value v;
+		ss >> v;
+		string err = get_last_error();
+		if (!err.empty()) {
+			cerr << err << endl;
+			return -1;
+		}
+		object& o = v.get<object>();
+		picojson::array& aAry = o["points"].get<picojson::array>();
+		cout << "points:" << endl;
+		for (i = 0; i < aAry.size(); i++)
+			cout << "\t" << aAry[i].get<string>() << endl;
+
+	/*for (picojson::array::iterator it = ary.begin(); it != ary.end(); it++)
+	{
+		cout << get<string> << " ";
+	}*/
+
 	picojson::object& obj = v.get<picojson::object>();
 	std::cout << "width: " << obj["width"].get<double>() << std::endl;
 	std::cout << "height: " << obj["height"].get<double>() << std::endl;
 	std::cout << "startedAtUnixTime: " << obj["startedAtUnixTime"].get<double>() << std::endl;
 	std::cout << "turn: " << obj["turn"].get<double>() << std::endl;
-	std::cout << "points: ";
-	picojson::points& ary = obj["points"].get<picojson::arraypoints>();
-	for (const auto& e : ary) {  // vectorをrange-based-forでまわしている。
-		std::cout << e.get<std::string>() << " ";
-	}
+	std::cout << "points" << obj["points"].get<picojson::array>().at(0);
 
 	return 0;
 }
