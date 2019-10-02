ff70bbe37c43a4b2a56dce452d29325058f850b3
Shimoyama <s.ayanamikai@gmail.com>
2019-10-03 03:53:06 +0900

    add
    描画する(フレーム・エージェント)関数の追加

--- procon30_kyogi/procon30_kyogi/gui/DrawMap.cpp
+++ procon30_kyogi/procon30_kyogi/gui/DrawMap.cpp
@@ -1,6 +1,46 @@
 #include"DrawMap.h"
 
-void DrawMap::drawMapFrame(){
-	Filed* filed;
+void DrawMap::drawMapFrame() {
+	Map* map;
+	map = map->getMap();
+	CreateMap create;
+	create.createMapFrame(map->vertical,map->width);
+}
+
+void DrawMap::drawMapState1() {
+	Map* map;
+	map = map->getMap();
+	CreateMap create;
+}
+
+void DrawMap::drawMapState2() {
+
+}
+
+void DrawMap::drawMapAgents1() {
+	Agents* agents;
+	agents = agents->getAgents;
+	CreateMap create;
+	for (int i = 0; i < agents->ourAgents.size(); i++) {
+		create.createMapAgent1(1, agents->ourAgents[i][0],agents->ourAgents[i][1]);
+		create.createMapAgent1(2, agents->otherAgents[i][0],agents->otherAgents[i][1]);
+	}
+}
+
+void DrawMap::drawMapAgents2() {
+	Agents* agents;
+	agents = agents->getAgents;
+	CreateMap create;
+	for (int i = 0; i < agents->ourAgents.size(); i++) {
+		create.createMapAgent2(1, agents->ourAgents[i][0], agents->ourAgents[i][1]);
+		create.createMapAgent2(2, agents->otherAgents[i][0], agents->otherAgents[i][1]);
+	}
+}
+
+void DrawMap::drawMapPoint1() {
+
+}
+
+void DrawMap::drawMapPoint2() {
 
 }
--- procon30_kyogi/procon30_kyogi/gui/DrawMap.h
+++ procon30_kyogi/procon30_kyogi/gui/DrawMap.h
@@ -1,9 +1,16 @@
 #include"../Data/Agents.h"
 #include"../Data/Field.h"
+#include"../Data/Map.h"
+#include"../gui/CreateMap.h"
 #pragma once
-class DrawMap
-{
+class DrawMap{
 public:
 	void drawMapFrame();
+	void drawMapState1();
+	void drawMapState2();
+	void drawMapAgents1();
+	void drawMapAgents2();
+	void drawMapPoint1();
+	void drawMapPoint2();
 };
 
