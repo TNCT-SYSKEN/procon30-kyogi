b05582187aec959f137376e9471addacdc5dc4b6
takishun <takishun.st3260@gmail.com>
2020-01-19 13:44:11 +0900

    Initialize/Redifineの追加

---
+++ procon30_kyogi/procon30_kyogi/Initialize/Redefine.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../SystemManager.h"
+
+class Redefine {
+public:
+	// called from DrawData.readTurnChange()•Ó‚è
+	//int beforeTurn ,int AfterTurn
+	void readTurnChange(int, int);
+
+};
